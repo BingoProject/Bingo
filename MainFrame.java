1a9d5d972b99b477c71acafa7f87169d8c3c6a9a
polk1200 <polk1200@naver.com>
2016-07-12 10:16:09 +0900

    바뀐 UI

--- MainFrame.java
+++ MainFrame.java
@@ -7,8 +7,9 @@ import javax.swing.event.*;
 
 public class MainFrame extends JFrame implements ActionListener{
 	private JButton[] btnArr = new JButton[25];
-	private JButton randomBtn;
-	private JPanel bingoPane /*memberPane*/;
+	private JButton[] btnArr2 = new JButton[25];
+	private JButton randomBtn, readyBtn;
+	private JPanel bingoPane, bingoPane2, readyPane /*memberPane*/;
 	
 	public void makeFrame(){
 		
@@ -18,35 +19,69 @@ public class MainFrame extends JFrame implements ActionListener{
 		//메인프레임 set하기
 		this.setTitle("빙고 게임");
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setBounds(200, 300, 1000, 600);
-		this.setLayout(new BorderLayout());
+		this.setBounds(300, 100, 1050, 600);
+		this.setLayout(null);
 		
 		
 		//빙고 판 set하기
+		//내 빙고판
 		bingoPane = new JPanel();
 		bingoPane.setBackground(Color.lightGray);
-		bingoPane.setSize(1000, 500);
-		bingoPane.setLocation(20, 20);
+		bingoPane.setLocation(0, 0);
 		bingoPane.setLayout(new GridLayout(5, 5));
+		bingoPane.setSize(500, 500);
+		
+		//상대편 빙고판
+		bingoPane2 = new JPanel();
+		bingoPane2.setBackground(Color.lightGray);
+		bingoPane2.setLocation(532, 0);
+		bingoPane2.setLayout(new GridLayout(5, 5));
+		bingoPane2.setSize(500, 500);
 		
 		for(int i = 0; i < btnArr.length; i++){
 			
 			btnArr[i] = new JButton("버튼");
+			btnArr2[i] = new JButton();
 			
 			btnArr[i].addActionListener(this);
+			
 			bingoPane.add(btnArr[i]);
+			bingoPane2.add(btnArr2[i]);
+			
 		}
-		this.add(bingoPane, BorderLayout.CENTER);
+		this.add(bingoPane2);
+		this.add(bingoPane);
+		
 		bingoPane.setVisible(true);
+		bingoPane2.setVisible(false);
 		
-		//자동 채우기 버튼
+		//자동 채우기 버튼 & 준비 버튼 
+		readyBtn = new JButton("준비 완료");
+		readyBtn.setFont(new Font(readyBtn.getText(), Font.BOLD, 30));
+		readyBtn.setBackground(Color.lightGray);
+		readyBtn.setEnabled(false);
+		
+		readyPane = new JPanel();
+		readyPane.setLayout(new GridLayout(1, 2));
+		readyPane.setSize(1035, 60);
+		readyPane.setLocation(0, 500);
+		
+		
 		randomBtn = new JButton("자동 채우기");
+		randomBtn.setFont(new Font(randomBtn.getText(), Font.BOLD, 30));
 		randomBtn.setBackground(Color.lightGray);
-		randomBtn.setSize(1000, 200);
 		
+		readyPane.add(randomBtn);
+		readyPane.add(readyBtn);
 		
 		randomBtn.addActionListener(this);
-		this.add(randomBtn, BorderLayout.SOUTH);
+		readyBtn.addActionListener(this);
+		
+		this.add(readyPane, BorderLayout.SOUTH);
+		/*this.add(readyBtn, BorderLayout.SOUTH);
+		this.add(randomBtn, BorderLayout.SOUTH);*/
+		
+		readyPane.setVisible(true);
 		randomBtn.setVisible(true);
 		/*//회원 관리 set하기
 		memberPane = new JPanel();
@@ -75,14 +110,37 @@ public class MainFrame extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// 자동 채우기 버튼
-		if(e.getActionCommand().equals(randomBtn.getText()))
+		if(e.getActionCommand().equals(randomBtn.getText())){
 			for(int i = 0; i < btnArr.length; i++){
-				btnArr[i].
+				int random = (int)(Math.random() * 50) + 1;
+				btnArr[i].setText(String.valueOf(random));
+				for(int j = 0; j < i; j++){
+					if(btnArr[j].getText().equals(String.valueOf(random))){
+						i--;
+						break;
+					}
+						
+				}
 				
 				
+				btnArr[i].setEnabled(false);
 			}
+			readyBtn.setEnabled(true);
+			randomBtn.setEnabled(false);
+		}
+		// 준비 완료 버튼
+		if(e.getActionCommand().equals(readyBtn.getText())){
+			readyBtn.setEnabled(false);
+			
+			for(int i = 0; i < btnArr.length; i++){
+				btnArr[i].setEnabled(true);
+			}
+				
+			
+		}
 		
 		
+			
 	}
 	
 }
